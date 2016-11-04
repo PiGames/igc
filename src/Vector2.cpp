680c739d0a17445fe073cd30299ec1b82a0e255d
mvxxx <mpatrycjusz@interia.pl>
2016-11-04 21:32:18 +0100

    Vector2
    -added first version of impementation of class Vector2

--- src/Vector2.cpp
+++ src/Vector2.cpp
@@ -1,1 +1,50 @@
 #include "Vector2.h"
+
+Vector2::Vector2()
+{
+}
+
+Vector2::Vector2(float _x, float _y)
+{
+	x = _x;
+	y = _y;
+}
+
+float Vector2::Magnitude()
+{
+
+}
+
+void Vector2::Normalize()
+{
+	float length = (sqrt(x*x + y*y));
+	x = x / length;
+	y = y / length;
+}
+
+Vector2 Vector2::GetNormalized()
+{
+	Vector2 normalisedVector(x, y);
+	normalisedVector.Normalize();
+	return normalisedVector;
+}
+
+Vector2 Vector2::operator+(const Vector2 & second)
+{
+	return Vector2(x+second.x,y+second.y);
+}
+
+Vector2 Vector2::operator-(const Vector2 & second)
+{
+	return Vector2(x - second.x, y - second.y);
+}
+
+Vector2 Vector2::operator*(const Vector2 & second)
+{
+	return Vector2(x * second.x, y * second.y);
+}
+
+Vector2 Vector2::operator/(const Vector2 & second)
+{
+	return Vector2(x / second.x, y / second.y);
+}
